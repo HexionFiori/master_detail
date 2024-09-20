ca463b1cf3e91d16723d5921922b0f088fa3bcba
Harish Wadhwani <harish.wadhwani@hexion.com>
2024-09-20 03:20:35 +0000

    Initial commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,27 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+],
+function (Controller, JSONModel) {
+    "use strict";
+
+    return Controller.extend("sample.app.zmasterdetail.controller.View1", {
+        onInit: function () {
+             this.getView().setModel(new JSONModel(), "Data");
+        },
+        onListItemPress: function(oEvent){
+            var oSelectedOrder = oEvent.getParameter("listItem").getBindingContext().getObject();
+            var sPath = `/Customers(CustomerID='${oSelectedOrder.CustomerID}')`
+            var oModel = this.getOwnerComponent().getModel();
+            oModel.read(sPath, {
+                success: function(data) {
+this.getView().getModel("Data").setProperty("/details", data);
+
+                }.bind(this),
+                error: () => {
+
+                }
+            })
+        }
+    });
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"sampleapp/zmasterdetail/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
